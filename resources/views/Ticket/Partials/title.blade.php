4d0efa4c65f3816a9a01a7f07170869b3dd189b1
gtbatun <gtbatun@gmail.com>
2024-05-27 04:13:48 -0500

    test
    test

--- resources/views/Ticket/Partials/title.blade.php
+++ resources/views/Ticket/Partials/title.blade.php
@@ -1,4 +1,4 @@
- <!-- <div style="max-height: 15px" class="pb-5">
+ <div style="max-height: 15px" class="pb-5">
     <div class="d-flex w-100 justify-content-between">
         <h5>{{ $ticket->usuario->name }}</h5>
         <small class="text-success">{{ $ticket->updated_at->diffForHumans() }}</small>
@@ -10,7 +10,7 @@
             <p  style="max-width: 300px; max-height: 15px;">{{ $ticket->title }}</p>
         </a>
     </div>
-</div> -->
+</div>
 <style>
     .status-enviado {
     color: blue;
@@ -32,15 +32,15 @@
     color: purple;
 }
 </style>
-<div class="direct-chat-msg" >    
+<!-- <div class="direct-chat-msg" >    
     <div class="direct-chat-infos clearfix">
     <a href="{{ route('ticket.show', $ticket) }}" title="{{ $ticket->description }}">
         <span class="text-success float-left" >{{ $ticket->title }}</span>
     </a>
-    <!-- <span class="{{ $messageClass }} float-right">{{$messageStatus}}</span>    -->
+    <span class="{{ $messageClass }} float-right">{{$messageStatus}}</span>   
     </div>
     <div class="direct-chat-infos">    
         <span class="direct-chat-name float-left">{{$ticket->usuario->name}}</span>
         <span class="float-right">{{$gestionTime->diffForHumans()}}</span>
     </div>
-</div>
+</div> -->
