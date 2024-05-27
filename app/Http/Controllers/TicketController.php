206c8b25864fe894c8de8a1ffd6fbf362fdd999a
gtbatun <80859756+gtbatun@users.noreply.github.com>
2024-05-27 08:10:00 -0500

    test
    test

--- app/Http/Controllers/TicketController.php
+++ app/Http/Controllers/TicketController.php
@@ -147,8 +147,8 @@ class TicketController extends Controller
             return [
                 'id' => $ticket->id,
                 // 'id' => $messageStatus,
-                'title' => view('Ticket.Partials.title', ['ticket' => $ticket])->render(),
-                // 'title' => view('Ticket.Partials.title', ['ticket' => $ticket,'messageStatus' => $messageStatus,'gestionTime' => $gestionTime,'messageClass' => $messageClass])->render(),
+                // 'title' => view('Ticket.Partials.title', ['ticket' => $ticket])->render(),
+                'title' => view('Ticket.Partials.title', ['ticket' => $ticket,'messageStatus' => $messageStatus,'gestionTime' => $gestionTime,'messageClass' => $messageClass])->render(),
                 'category' => $ticket->category->name,
                 'sucursal' => $ticket->usuario->sucursal->name,
                 // 'department' => $ticket->department->name,
