d7f75e6dfff21ca248b4527bfbb7e29d6bf98303
gtbatun <80859756+gtbatun@users.noreply.github.com>
2024-08-30 18:50:27 -0500

    titile de ticket
    se habilita  un txto y un if para visualizar quien contesto el ticket la ultima vez

--- resources/views/Ticket/Partials/title.blade.php
+++ resources/views/Ticket/Partials/title.blade.php
@@ -28,21 +28,13 @@
         title="{{ $ticket->description }}">        
         <span>{{ $ticket->title }}</span>
     </a>
-    
-    
-        <!-- <div class="form-group">    -->
-        @if($notifications > 0)           
-        <span class="badge rounded-circle bg-danger  float-right"> {{$notifications}}</span>  
-        <i style="color: green; font-size: 20px;" class="fas fa-comments float-right">
-            <!-- <span class="badge rounded-circle bg-success position-relative "> {{$notifications}}</span> -->
-        </i>
-           <!-- <span type="button" class="btn btn-primary rounded-pill float-right"><i class="fas fa-comments"></i>
-                <span class="badge bg-secondary ">{{$notifications}}</span>
-        </span> -->
-        @endif
-        <!-- <span class="{{ $messageClass }} float-right">{{$messageStatus}}  <i class="fas fa-comments"></i>3</span> -->
-        <!-- </div> -->
-       
+    @if($notifications > 0)           
+    <span class="badge rounded-circle bg-danger  float-right"> {{$notifications}}</span>  
+    <i style="color: green; font-size: 20px;" class="fas fa-comments float-right">
+    </i>
+    @else
+    <span class="{{ $messageClass }} float-right">{{$messageStatus}}  <i class="fas fa-comments"></i>3</span>
+    @endif       
     </div>
     <div class="direct-chat-infos">    
         <span class="direct-chat-name float-left">{{$ticket->usuario->name}}</span>
